6304d04d1f9dd25e979b3e28faf41ab255f33c3f
andrea <andry_-900@hotmail.it>
2020-03-09 23:03:04 +0100

    Presi valori profilo da DB

--- app/src/main/java/com/example/macc/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/macc/ui/profile/ProfileFragment.java
@@ -1,6 +1,5 @@
 package com.example.macc.ui.profile;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,12 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import com.example.macc.NavigationActivity;
 import com.example.macc.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Objects;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,39 +34,88 @@ public class ProfileFragment extends Fragment {
         Button profile_saveButton = root.findViewById(R.id.profile_btnSave);
 
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        String[] displayName;
 
         if (firebaseUser != null) {
+            String[] displayName;
             String provider = firebaseUser.getProviderData().get(1).getProviderId();
+            DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+            Query query = rootRef.child("users").orderByChild("id").equalTo(firebaseUser.getUid());
 
             if (provider.equals("facebook.com") || provider.equals("google.com")) {
-                if (!Objects.equals(firebaseUser.getDisplayName(), "")) {
-                    displayName = Objects.requireNonNull(firebaseUser.getDisplayName()).split(" ", 2);
+                if (firebaseUser.getDisplayName() != null && !firebaseUser.getDisplayName().equals("")) {
+                    displayName = firebaseUser.getDisplayName().split(" ", 2);
                     profile_TextViewName.setText(displayName[0]);
                     profile_TextViewSurname.setText(displayName[1]);
-                }else{
+                } else {
                     profile_labViewName.setVisibility(View.INVISIBLE);
                     profile_labViewSurname.setVisibility(View.INVISIBLE);
                 }
-            } else {
-                profile_TextViewName.setText("instance.getInstance().getName()");
-                profile_TextViewSurname.setText("instance.getInstance().getSurname()");
-            }
-            profile_TextViewEmail.setText(firebaseUser.getEmail());
-        }
 
-        //onClick on button Save
-        profile_saveButton.setOnClickListener(v -> {
-            if (profile_edDepartment.getText().toString().equals("")) {
-                profile_edDepartment.setError("please enter a department!");
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            String university = snapshot.child("university").getValue(String.class);
+                            String department = snapshot.child("department").getValue(String.class);
+
+                            profile_edDepartment.setText(department);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             } else {
-                String newHint = profile_edDepartment.getText().toString();
-                profile_edDepartment.getText().clear();
-                profile_edDepartment.setHint(newHint);
-                Intent intent = new Intent(getContext(), NavigationActivity.class);
-                startActivity(intent);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            String name = snapshot.child("name").getValue(String.class);
+                            String surname = snapshot.child("surname").getValue(String.class);
+                            String university = snapshot.child("university").getValue(String.class);
+                            String department = snapshot.child("department").getValue(String.class);
+
+                            profile_edDepartment.setText(department);
+                            profile_TextViewName.setText(name);
+                            profile_TextViewSurname.setText(surname);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
-        });
+
+            profile_TextViewEmail.setText(firebaseUser.getEmail());
+
+            //onClick on button Save
+            profile_saveButton.setOnClickListener(v -> {
+                if (profile_edDepartment.getText().toString().equals("")) {
+                    profile_edDepartment.setError("Please enter a department!");
+                } else {
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                String university = snapshot.child("university").getValue(String.class);
+                                String department = snapshot.child("department").getValue(String.class);
+
+                                // DA FARE UPDATE UNIVERSITA' E DIPARTIMENTO
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            });
+        }
         return root;
     }
 }
