d63bf9e5096ee1a8e63bd235586ac72a65899ca6
Manzo <manzaramanuel@gmail.com>
2020-03-09 18:59:48 +0100

    working on profile fragment

--- app/src/main/java/com/example/macc/NavigationActivity.java
+++ app/src/main/java/com/example/macc/NavigationActivity.java
@@ -42,9 +42,7 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
--- app/src/main/java/com/example/macc/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/macc/ui/profile/ProfileFragment.java
@@ -1,35 +1,71 @@
 package com.example.macc.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.example.macc.NavigationActivity;
 import com.example.macc.R;
+import com.example.macc.Users;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel profileViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(this, new Observer<String>() {
+
+        TextView profile_TextViewName = root.findViewById(R.id.profile_TextViewName);
+        TextView profile_TextViewSurname = root.findViewById(R.id.profile_TextViewSurname);
+        TextView profile_TextViewEmail = root.findViewById(R.id.profile_TextViewEmail);
+        EditText profile_edDepartment = root.findViewById(R.id.profile_edDepartment);
+        Button profile_saveButton = root.findViewById(R.id.profile_btnSave);
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        String[] displayName;
+
+        if(firebaseUser != null){
+            String provider = firebaseUser.getProviderData().get(1).getProviderId();
+
+            if ( provider.equals("facebook.com") || provider.equals("google.com")){
+                displayName = firebaseUser.getDisplayName().split(" ",2);
+                profile_TextViewName.setText(displayName[0]);
+                profile_TextViewSurname.setText(displayName[1]);
+                profile_TextViewEmail.setText(firebaseUser.getEmail());
+            }else{
+                profile_TextViewName.setText("instance.getInstance().getName()");
+                profile_TextViewSurname.setText("instance.getInstance().getSurname()");
+                profile_TextViewEmail.setText("instance.getInstance().getEmail()");
+        }
+        }
+
+        //onClick on button Save
+        profile_saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                if (profile_edDepartment.getText().toString().equals("")){
+                    profile_edDepartment.setError("please enter a department!");
+                }else{
+                    String newHint = profile_edDepartment.getText().toString();
+                    profile_edDepartment.getText().clear();
+                    profile_edDepartment.setHint(newHint);
+                    Intent intent = new Intent(getContext(), NavigationActivity.class);
+                    startActivity(intent);
+                }
             }
         });
         return root;
     }
+
 }
+
