cf39c5fa61f91433112852b7dab70bfc41523c0e
Manzo <manzaramanuel@gmail.com>
2020-03-30 16:05:20 +0200

    small changes pt2

--- app/src/main/java/com/example/macc/ui/information/InformationFragment.java
+++ app/src/main/java/com/example/macc/ui/information/InformationFragment.java
@@ -14,22 +14,37 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.macc.R;
 
-public class InformationFragment extends Fragment {
+import org.w3c.dom.Text;
 
-    private InformationViewModel informationViewModel;
+public class InformationFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        informationViewModel =
-                ViewModelProviders.of(this).get(InformationViewModel.class);
         View root = inflater.inflate(R.layout.fragment_information, container, false);
-        final TextView textView = root.findViewById(R.id.text_information);
-        informationViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        TextView textView_desc = root.findViewById(R.id.text_information_desc);
+        TextView textView_creators = root.findViewById(R.id.text_information_creators_desc);
+        TextView textView_version = root.findViewById(R.id.text_information_version_desc);
+        String description = "Welcome in Virgilio! \n Virgilio is an application designed to help students choose which exam to include " +
+                "in their study plan. The name \"Virgilio\" was chosen specifically to give the student the idea" +
+                "of having a sort of guardian angel who guides him in his university choices, just like for  Dante" +
+                "Alighieri in the work of the Divine Comedy.\nIn fact, how many times a student finds himself in the condition "+
+                "of not knowing which exam to insert in his course of study and relies on the reviews of students who have " +
+                "previously taken it. Thanks to Virgilio, each student will have information on one or more exams" +
+                " related to his department and his university written by other students in completely anonymous form." +
+                "The student can insert reviews on the exams he has taken with the possibility of specifying" +
+                " the grade with which the exam was passed, a value that indicates how interesting the topics covered" +
+                " in the course were and finally a general comment that advises whether or not the inclusion of the exam" +
+                " in your study plan. ";
+
+        String creators = "bellia.1586420@studenti.uniroma1.it \n" +
+                "manzara.1876891@studenti.uniroma1.it";
+
+        String version = "1.0.0";
+
+        textView_desc.setText(description);
+        textView_creators.setText(creators);
+        textView_version.setText(version);
+
         return root;
     }
 }
